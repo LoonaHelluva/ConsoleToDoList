548697771a61427b7fdfb75aba133c5dac662c9d
Mussa Kurtseitov <mussakurtseitov@gmail.com>
2026-07-25 01:37:31 +0300

    Start of the project

---
+++ test1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(Add(5, 10));
+    }
+
+    static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
